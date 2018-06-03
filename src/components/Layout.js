e10eb1896194696ea1e8a4f491be69871c274b75
jakmak24 <jakub.makselon@gmail.com>
2018-06-03 11:47:26 +0200

    sticy nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import Header from "./Header"
-import Body from "./Body"
+import SideBar from "./SideBar"
+import MainView from "./MainView"
 
 export default class Layout extends React.Component {
 
@@ -10,7 +11,8 @@ export default class Layout extends React.Component {
     return (
       <div className = "Layout">
         <Header />
-        <Body />
+        <SideBar/>
+        <MainView />
       </div>
     );
   }
