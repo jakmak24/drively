a2c5cf4f72a21bbfe6b5652a263be62ed438fead
lisu9090 <lisu9090@gmail.com>
2018-06-07 21:32:33 +0200

    Stworzono komponent QuickAdd

--- src/components/QuickAdd.js
+++ src/components/QuickAdd.js
@@ -5,8 +5,24 @@ import "../css/QuickAdd.css";
 export default class QuickAdd extends React.Component {
   render() {
     return (
-      <div className ="QuickAdd">
-        QuickAdd
+      <div className ="QuickAdd clearfix">
+        <p className="Title">QuickAdd</p>
+        <div className="QuickOptions">
+          <p>Refuel</p>
+          <img src={window.location.origin + '/icons/refuel.svg'} alt="Refuel"></img>
+        </div>
+        <div className="QuickOptions">
+          <p>Refuel</p>
+          <img src={window.location.origin + '/icons/service.svg'} alt="Service"></img>
+        </div>
+        <div className="QuickOptions">
+          <p>Refuel</p>
+          <img src={window.location.origin + '/icons/payment.svg'} alt="Payment"></img>
+        </div>
+        <div className="QuickOptions">
+          <p>Refuel</p>
+          <img src={window.location.origin + '/icons/reminder.png'} alt="Reminder"></img>
+        </div>
       </div>
     );
   }
