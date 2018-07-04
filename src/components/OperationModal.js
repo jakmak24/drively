b145bf17e1e1acd08a2ffb22053b33b310a24203
lisu9090 <lisu9090@gmail.com>
2018-07-05 00:30:52 +0200

    Zmodyfikowano modal aby można było nim zarządzać.

---
+++ src/components/OperationModal.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import "../css/modal.css"
+
+export default class OperationModal extends React.Component {
+  render(){
+    return (
+      <div className="OperationModal">
+        <div className="modal-form">
+          <div className = "form-element">
+            <label className="form-label">Date</label>
+            <div className = "input-div">
+              <input type="text"></input>
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Name</label>
+            <div className = "input-div">
+              <select>Wymiana opon</select>
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Cost</label>
+            <div className = "input-div">
+              <input type="text"></input>
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Place</label>
+            <div className = "input-div">
+              <input type="text"></input>
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Odometer</label>
+            <div className = "input-div">
+              <input type="text"></input>
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Reminder</label>
+            <div className = "input-div">
+              <label className="switch">
+                <input type="checkbox"></input>
+                <span className="slider round"></span>
+              </label>
+
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Notes</label>
+            <div className = "input-div">
+              <input type="text"></input>
+            </div>
+          </div>
+      </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/ReminderModal.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import "../css/modal.css"
+
+export default class ReminderModal extends React.Component {
+  render(){
+    return(
+      <div className="ReminderModal">
+        <div className="modal-form">
+          <div className = "form-element">
+            <label className="form-label">Date</label>
+            <div className = "input-div">
+              <input type="text"></input>
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Name</label>
+            <div className = "input-div">
+              <select >
+              <option value="opony">Wymiana opon</option>
+              </select>
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Cost</label>
+            <div className = "input-div">
+              <input type="text"></input>PLN
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Place</label>
+            <div className = "input-div">
+              <input type="text"></input>
+            </div>
+          </div>
+
+          <div className = "form-element">
+            <label className="form-label">Notes</label>
+            <div className = "input-div">
+              <input type="text"></input>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
